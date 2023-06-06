da7821e549d55564613524afb9a7e18f0b5eb049
Tim <timmyway1990@gmail.com>
2023-06-06 13:17:57 +0300

    feat: Initialization of the first project.
    - Removed all unnecessary files to create a clean repository.
    - Created first Title component.
    - Created a projects folder containing all future projects.
    
    I'd like to install sass and tailwind css before continuing with the 1st project.

---
+++ src/projects/01-e-signature/EsignatureApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "../../components/Title";
+
+export default function EsignatureApp() {
+    return (
+        <div className="container text-center">
+            <Title classes={'title'} text={ "Un jour je serai le meilleur dresseur !" }></Title>
+            <Title classes={'subtitle'} text={ "Je me battrai sans répi" }></Title>
+        </div>
+    )
+}
